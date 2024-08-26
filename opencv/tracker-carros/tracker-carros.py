1e9af7cc6acda3663fc510a3ee8dad9a3719ecbe
Lucas Riffel <lucas26.riffel@gmail.com>
2024-08-25 21:51:35 -0300

    upload

--- opencv/tracker-carros/tracker-carros.py
+++ opencv/tracker-carros/tracker-carros.py
@@ -6,7 +6,7 @@ object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=4
 while True:
     ret, frame = cap.read()
 
-    roi = frame[ 180 : 360, 250 : 450]
+    roi = frame[ 120 : 360, 0 : 260]
 
     mask = object_detector.apply(roi)
     _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
